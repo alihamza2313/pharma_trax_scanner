7afe6ca339ef745ae4764c1bb7292ac102f81bf4
Ahmed Naeemgit config --get-all user.namegit config --get-all user.nameclearcd <ahmednaeem1012@gmail.com>
2022-06-30 04:41:11 -0700

    okk

--- lib/Widgets/app_drawer.dart
+++ lib/Widgets/app_drawer.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pharma_trax_scanner/Widgets/about_pharma.dart';
-import "../Widgets/about_pharma.dart";
-import '../Widgets/line_equipment.dart';
-import 'how_it_works.dart';
+//  import 'package:pharma_trax_scanner/screens/line_equipment.dart';
 import '../Widgets/line_level_hardware.dart';
-
 import '../utils/colors.dart';
 import 'How_it_works.dart';
 import 'line_equipment.dart';
@@ -115,7 +112,6 @@ class AppDrawer extends StatelessWidget {
           ),
           title: const Text("Line Level Hardware"),
           onTap: () {
-            Navigator.of(context).pop();
             Navigator.of(context)
                 .pushReplacementNamed(LineLevelHardware.routeName);
           },
@@ -131,7 +127,6 @@ class AppDrawer extends StatelessWidget {
           ),
           title: const Text("Line Equipment"),
           onTap: () {
-            Navigator.of(context).pop();
             Navigator.of(context)
                 .pushReplacementNamed(Line_equipment.routeName);
           },
@@ -147,9 +142,8 @@ class AppDrawer extends StatelessWidget {
           ),
           title: const Text("How it Works"),
           onTap: () {
-            Navigator.of(context).pop();
             Navigator.of(context).pushReplacementNamed(How_it_works.routeName);
-            // print('How_it_works');
+            print('How_it_works');
           },
         ),
         ListTile(
@@ -164,15 +158,11 @@ class AppDrawer extends StatelessWidget {
           title: const Text("About Pharma Trax"),
           onTap: () {
             Navigator.of(context).pop();
-<<<<<<< HEAD
             Navigator.of(context).push(
               MaterialPageRoute(
                 builder: ((context) => const About_pharma()),
               ),
             );
-=======
-            Navigator.of(context).pushNamed(About_pharma.routeName);
->>>>>>> origin/main
           },
         ),
       ]),
