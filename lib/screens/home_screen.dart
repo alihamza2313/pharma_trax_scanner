e7baedbebf710078559fc175e5d2e0395a24906c
Ahmed Naeemgit config --get-all user.namegit config --get-all user.nameclearcd <ahmednaeem1012@gmail.com>
2022-07-25 03:19:52 -0700

    ss

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,7 +42,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     getSharePrefenceValue();
 
-    SaveValueInPrefecnce();
+    // SaveValueInPrefecnce();
     super.initState();
   }
 
